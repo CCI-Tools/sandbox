9c6846bc9c6eb996b85179451f07a2314e92a7d0
Janis Gailis <gailis@stcorp.no>
2017-01-18 18:13:01 +0100

    Testing suggestions by Ryan

--- xarray/daily_sbox.py
+++ xarray/daily_sbox.py
@@ -11,16 +11,20 @@ monitor = ConsoleMonitor()
 data_store = DATA_STORE_REGISTRY.get_data_store('esa_cci_odp')
 
 print('1')
-sst = xr.open_mfdataset('/home/ccitbx/Desktop/sst/*.nc', chunks = {'lat':36,
-                                                              'lon':72,
-                                                              'time':31},
-                        concat_dim = 'time')
+# sst = xr.open_mfdataset('/home/ccitbx/Desktop/sst/*.nc', chunks = {'lat':36,
+#                                                             'lon':72,
+#                                                             'time':31},
+#                       concat_dim = 'time')
+sst = xr.open_mfdataset('/home/ccitbx/Desktop/sst/*.nc', concat_dim = 'time')
+print(sst.nbytes * (2 ** -30))
 print('2')
 
-sm = xr.open_mfdataset('/home/ccitbx/Desktop/sm/*.nc', chunks = {'lat':72,
-                                                            'lon':144,
-                                                            'time':31},
-                       concat_dim = 'time')
+# sm = xr.open_mfdataset('/home/ccitbx/Desktop/sm/*.nc', chunks = {'lat':72,
+#                                                           'lon':144,
+#                                                           'time':31},
+#                      concat_dim = 'time')
+sm = xr.open_mfdataset('/home/ccitbx/Desktop/sm/*.nc', concat_dim = 'time')
+print(sm.nbytes * (2 ** -30))
 print('3')
 
 sm_mean = sm.mean('time', keep_attrs=True, skipna=True)
@@ -36,9 +40,15 @@ print('7')
 sst_mean_time = xr.concat([sst_mean], t_dim)
 print('8')
 
-ops.save_dataset(sm_mean_time, '/home/ccitbx/Desktop/sm_mean.nc')
+# ops.save_dataset(sm_mean_time, '/home/ccitbx/Desktop/sm_mean.nc')
+sm_mean_time.load()
+print('8.1')
+sm_mean_time.to_netcdf('/home/ccitbx/Desktop/sm_mean.nc')
 print('9')
-ops.save_dataset(sst_mean_time, '/home/ccitbx/Desktop/sst_mean.nc')
+# ops.save_dataset(sst_mean_time, '/home/ccitbx/Desktop/sst_mean.nc')
+sst_mean_time.load()
+print('9.1')
+sst_mean_time.to_netcdf('/home/ccitbx/Desktop/sst_mean.nc')
 print('10')
 print('Elapsed time: {}'.format(time.time()-t))
 
